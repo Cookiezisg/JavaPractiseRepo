023cc4fb0012b54f3250c212e0455974af06cb38
Cookiezisg <sunweilin.sg@gmail.com>
2024-12-11 22:56:16 +0800

    11 Dec 2024

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// File: Main.java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+class Helper {
+    public void help() {
+        System.out.println("Helping...");
+    }
+}
