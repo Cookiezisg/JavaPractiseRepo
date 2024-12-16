d2d9002a0b5b836996096c9bbd2d678ef7035b20
Cookiezisg <sunweilin.sg@gmail.com>
2024-12-17 00:07:13 +0800

    17 Dec 2024

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Main {
+
+    static void staticVoidMethod(){
+        System.out.println("this is static void method");
+    }
+
     public static void main(String[] args){
         System.out.println("This is the main method");
+        Class classClass = new Class(10);
+        System.out.println(classClass.x);
+
+
+        
     }
 }
---
+++ src/mypack/MyPackage.java
@@ -0,0 +1,11 @@
+package mypack;
+
+public class MyPackage {
+    public static void yeild(){
+        System.out.println("this is MyPackage!");
+    }
+
+    public static void main(String[] args){
+        System.out.println("this is MyPackage!");
+    }
+}
