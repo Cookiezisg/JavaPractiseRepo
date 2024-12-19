543c17caa4e116dd4d593825e3e8604501c96bf7
Cookiezisg <sunweilin.sg@gmail.com>
2024-12-19 20:23:19 +0800

    19 Dec 2024

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
-class OuterClass {
-    int x = 10;
-  
-    static class InnerClass {
-      int y = 5;
-    }
+public class Main {
+  int x;
+  int y;
+  public Main(int x1, int y1){
+    x = x1;
+    y = y1;
   }
-  
-  public class Main {
-    public static void main(String[] args) {
-      OuterClass.InnerClass myInner = new OuterClass.InnerClass();
-      System.out.println(myInner.y);
-    }
+
+  public static void main(String[] args){
+    Main classobj = new Main(12, 20);
+    System.out.println(classobj.x);
+    System.out.println(classobj.y);
   }
+}
